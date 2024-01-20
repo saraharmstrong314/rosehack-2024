6f7ecd28c9ff72d1a86f38bc743f745e0a9c878a
✧༺♥༻∞ <jazzy@ucr-guest-03-10-14-9-235.wnet.ucr.edu>
2024-01-20 15:13:41 -0800

    coup revise 3

--- src/Coupons.js
+++ src/Coupons.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import "./Coupons.css"
 function Coupons() {
   return (
     <div className='container'>
      <h1>Coupons available</h1>
-     <p>Coupons made for you!</p>
+     <p className='para'>Coupons made for you!</p>
     <a href="https://www.groupon.com/coupons"> groupon</a>      
      <a href="https://www.retailmenot.com/">retailmenot</a>
      <a href="https://www.joinhoney.com/"> honey</a>
