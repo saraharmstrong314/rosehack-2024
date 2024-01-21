6946295307e803accdc7f187a7c98c21589a81a1
Jasmine Temporary Account <jasminetemporaryaccount@ucr-guest-03-10-14-10-125.wnet.ucr.edu>
2024-01-21 05:26:16 -0800

    real final commit

---
+++ src/nav-bar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+return (
+    <nav>
+    <ul>
+        <li><a href="src/nanami.js">financial Assistant</a></li>
+        <li><a href="src/nanami.js">financial Assistant</a></li>
+        
+        
+    </ul>
+    </nav>
+    );
+}
+
+export default Navbar;
